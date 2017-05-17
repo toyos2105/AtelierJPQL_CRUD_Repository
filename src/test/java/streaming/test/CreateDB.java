2920e4fcf848563ffd686352fdabab11f65b0c5d
quicotte-formation <>
2017-05-17 10:41:10 +0200

    MAJ Pour gérer les CrudRepository

--- src/test/java/streaming/test/CreateDB.java
+++ src/test/java/streaming/test/CreateDB.java
@@ -5,20 +5,23 @@
  */
 package streaming.test;
 
-import javax.persistence.Persistence;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import streaming.config.SpringConfig;
 
 /**
  *
  * @author tom
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes=SpringConfig.class)
 public class CreateDB {
     
 
     @Test
     public void test(){
-        Persistence.createEntityManagerFactory("PU").createEntityManager();
     }
     
 }
---
+++ src/test/java/streaming/test/PersonneDAOTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import streaming.config.SpringConfig;
+
+/**
+ *
+ * @author formation
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes=SpringConfig.class)
+public class PersonneDAOTest {
+    
+    @Test
+    public void test(){
+        
+    }
+}
