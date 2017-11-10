45ebb42223fb4d86ab071fe72994e09bb1d207f3
Formation <Formation@10.18.0.76>
2017-11-10 16:49:37 +0100

    v0.1

--- src/test/java/streaming/test/NewEmptyJUnitTest.java
+++ src/test/java/streaming/test/NewEmptyJUnitTest.java
@@ -8,6 +8,10 @@ package streaming.test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import org.junit.Assert;
+import org.junit.Test;
+import streaming.entity.Film;
 
 /**
  *
@@ -17,14 +21,18 @@ public class NewEmptyJUnitTest {
     EntityManagerFactory myPersistence = Persistence.createEntityManagerFactory("PU");
     EntityManager em = myPersistence.createEntityManager();
     
+    @Test
     public void Exercice() {
         em.getTransaction().begin();
         
+        Query query = em.createQuery("SELECT f FROM Film f WHERE f.id=4");
+        Film film = (Film) query.getSingleResult();
+
+        Assert.assertEquals("Fargo", film.getTitre());
         
+        System.out.println(film.getTitre());
         
-        
-        
-        em.getTransaction().commit();
+        //em.getTransaction().commit();
         
         
     }
