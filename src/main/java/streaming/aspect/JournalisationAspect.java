55ad5545da3a52aa2d844fbece9f1a01b27a0919
Formation <Formation@10.18.0.76>
2017-11-22 09:49:12 +0100

    - v4.4

--- src/main/java/streaming/aspect/JournalisationAspect.java
+++ src/main/java/streaming/aspect/JournalisationAspect.java
@@ -24,13 +24,13 @@ public class JournalisationAspect {
     @Autowired
     private JournalService service;
     
-    @Before("execution( * streaming.service.*.ajouter(..) )")
+    @Before("execution( * streaming.service.Service2.ajouter(..) ) or execution( * streaming.service.Service2.ajouter(..) )")
     // Avant l'exécution ce ce qui est entre parenthèses
     // le 1er * dit que je traite tout ce qui est retourné.
     // et on l'applique au reste de la parenthèse
     // On réalise la fonction en dessous
     public void avant(JoinPoint jp){
-        
-        throw new RuntimeException("Operation interdite");
+        System.err.println("In Avant");
+        //throw new RuntimeException("Operation interdite");
     }
 }
